42e28e0ae69f66c586fc1cc95c7ccae19b916264
vanijor <vanijor@hotmail.com>
2025-04-05 09:24:24 -0300

    CRUD completo

--- src/controllers/UsersControllers.ts
+++ src/controllers/UsersControllers.ts
@@ -4,6 +4,7 @@ import express, { Request, Response } from 'express';
 import { AppDataSource } from '../data-source';
 //Importar entidade
 import { User } from '../entity/User';
+import { Not } from 'typeorm';
 //Criar a aplicação Express
 const router = express.Router();
 
@@ -23,6 +24,41 @@ router.get('/users', async (req: Request, res: Response) => {
     //Retornar erro em caso de falha
     res.status(500).json({
       message: 'Erro em listar os usuários!',
+    })
+    return
+  }
+})
+
+//Criar a rota para visualizar um usuário
+//Endereço para acessar a api atraves da aplicacao externa com o verbo GET:http://localhost:8080/users/id
+router.get('/users/:id', async (req: Request, res: Response) => {
+  try {
+    //Obter o ID do usuário a partir dos parametros da requisição
+    const { id } = req.params;
+    
+    //Obter o repositorio da entidade Useer
+    const userRepository = AppDataSource.getRepository(User);
+    
+    //Buscar o usuário no banco de dados
+    const user = await userRepository.findOneBy({ id:parseInt(id) });
+    
+    //Verificar se o usuário foi encontrado
+    if (!user) {
+      res.status(404).json({
+        message: 'Usuário não encontrado!',
+      })
+      return      
+    }
+    
+    res.status(200).json({
+      user: user,
+    })
+    return      
+
+  } catch (error) {
+    //Retornar erro em caso de falha
+    res.status(500).json({
+      message: 'Erro ao visualizar o usuário!',
     })
     return
   }
@@ -77,3 +113,69 @@ router.post('/users', async (req: Request, res: Response) => {
 
 //Exportar a instrução que esta dentro da constante router
 export default router;
+
+//Criar a rota para editar usuário
+//Endereço para acessar a api atraves da aplicacao externa com o verbo PUT:http://localhost:8080/users/id
+//A aplicação externa deve indicar que está enviando os dados em formato de objeto: Content-Type: application/json
+/*
+{
+  "name": "Jorge"
+  "email": "jorge@jorge.com"
+}
+*/
+
+router.put('/users/:id', async (req: Request, res: Response) => {
+  try {
+    // Obter o ID do usuário a partir dos parametros da requisição
+    const { id } = req.params;
+
+    //Receber os dados enviados no corpo da requisição
+    const data = req.body;
+
+    //Obter o repositorio da entidade User
+    const userRepository = AppDataSource.getRepository(User);
+
+    //Buscar o usuário no banco de dados
+    const user = await userRepository.findOneBy({id: parseInt(id)})
+
+    //Verificar se o usuário foi encontrado
+    if (!user) {
+      res.status(404).json({
+        message: 'Usuário não encontrado!',
+      })
+      return
+    } 
+
+    //Verificar se já existe outro usuário com o mesmo email, mas que não seja o registro atual
+    const existingUser = await userRepository.findOne({
+      where: {
+        email: data.email,
+        id: Not(parseInt(id)), //Exclui o próprio resgistro da busca
+      }
+    })
+
+    //Verificar se o usuário foi encontrado
+    if (existingUser) {
+      res.status(400).json({
+        message: 'Já existe um usuário com esse email!',
+      })
+    }
+
+    //Atualizar os dados do usuário
+    userRepository.merge(user, data)
+
+    //Salvar as alterações no banco de dados
+    const updateUser = await userRepository.save(user)
+
+    res.status(200).json({
+      message: 'Usuário atualizado com sucesso!',
+      data: updateUser,
+    })
+
+  } catch (error) {
+    //Retornar erro em caso de falha
+    res.status(500).json({
+      message: 'Erro em editar o usuário!',
+    })
+  }
+})
