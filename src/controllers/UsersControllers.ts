7c3e6c76d134c572fba7172491aa4f3f81a069da
vanijor <vanijor@hotmail.com>
2025-04-05 09:47:55 -0300

    CRUD completo atualizado

--- src/controllers/UsersControllers.ts
+++ src/controllers/UsersControllers.ts
@@ -1,68 +1,66 @@
 //Importar a biblioteca Express
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 //Importar a conexão
-import { AppDataSource } from '../data-source';
+import { AppDataSource } from "../data-source";
 //Importar entidade
-import { User } from '../entity/User';
-import { Not } from 'typeorm';
+import { User } from "../entity/User";
+import { Not } from "typeorm";
 //Criar a aplicação Express
 const router = express.Router();
 
 //Criar a rota para listar usuários
 //Endereço para acessar a api atraves da aplicacao externa com o verbo GET:http://localhost:8080/users
-router.get('/users', async (req: Request, res: Response) => {
+router.get("/users", async (req: Request, res: Response) => {
   try {
     //Criar uma instância do repositorio de User
     const userRepository = AppDataSource.getRepository(User);
     //Recupera todos os usuários do banco de dados
     const users = await userRepository.find();
     //Retorna os usuários como resposta
-    res.status(200).json(users)
-    return
-
+    res.status(200).json(users);
+    return;
   } catch (error) {
     //Retornar erro em caso de falha
     res.status(500).json({
-      message: 'Erro em listar os usuários!',
-    })
-    return
+      message: "Erro em listar os usuários!",
+    });
+    return;
   }
-})
+});
 
 //Criar a rota para visualizar um usuário
 //Endereço para acessar a api atraves da aplicacao externa com o verbo GET:http://localhost:8080/users/id
-router.get('/users/:id', async (req: Request, res: Response) => {
+router.get("/users/:id", async (req: Request, res: Response) => {
   try {
     //Obter o ID do usuário a partir dos parametros da requisição
     const { id } = req.params;
-    
+
     //Obter o repositorio da entidade Useer
     const userRepository = AppDataSource.getRepository(User);
-    
+
     //Buscar o usuário no banco de dados
-    const user = await userRepository.findOneBy({ id:parseInt(id) });
-    
+    const user = await userRepository.findOneBy({ id: parseInt(id) });
+
     //Verificar se o usuário foi encontrado
     if (!user) {
       res.status(404).json({
-        message: 'Usuário não encontrado!',
-      })
-      return      
+        message: "Usuário não encontrado!",
+      });
+      return;
     }
-    
+
     res.status(200).json({
       user: user,
-    })
-    return      
-
+    });
+    return;
   } catch (error) {
     //Retornar erro em caso de falha
     res.status(500).json({
-      message: 'Erro ao visualizar o usuário!',
-    })
-    return
+      message: "Erro ao visualizar o usuário!",
+    });
+    return;
   }
-})
+});
 
 //Criar a rota para cadastrar usuário
 //Endereço para acessar a api atraves da aplicacao externa com o verbo POST:http://localhost:8080/users
@@ -74,45 +72,43 @@ router.get('/users/:id', async (req: Request, res: Response) => {
 }
 */
 
-router.post('/users', async (req: Request, res: Response) => {
+router.post("/users", async (req: Request, res: Response) => {
   try {
     //Receber os dados enviados no corpo da requisição
-    var data =req.body
+    var data = req.body;
 
     //Criar uma instancia do repositorio User
     const userRepository = AppDataSource.getRepository(User);
 
     //Recuperar o registro do banco de dados com o valor da coluna email
-    const existingUser = await userRepository.findOne({ where: { email: data.email } });
+    const existingUser = await userRepository.findOne({
+      where: { email: data.email },
+    });
 
     if (existingUser) {
       res.status(400).json({
-        message: 'Já existe usuário cadastrado com esse email',
-      })
-      return
+        message: "Já existe usuário cadastrado com esse email",
+      });
+      return;
     }
     //Criar um novo registro
-    const newUser = userRepository.create(data)
+    const newUser = userRepository.create(data);
 
     //Salvar o registro no banco de dados
     await userRepository.save(newUser);
 
     //Retornar resposta de sucesso
     res.status(201).json({
-      message: 'Usuário cadastrado com sucesso',
+      message: "Usuário cadastrado com sucesso",
       user: newUser,
-    })
-    
-  } catch(error) {
+    });
+  } catch (error) {
     //Retornar erro em caso de falha
     res.status(500).json({
-      message: 'Erro em cadastrar o usuário!',
-    })
+      message: "Erro em cadastrar o usuário!",
+    });
   }
-})
-
-//Exportar a instrução que esta dentro da constante router
-export default router;
+});
 
 //Criar a rota para editar usuário
 //Endereço para acessar a api atraves da aplicacao externa com o verbo PUT:http://localhost:8080/users/id
@@ -121,10 +117,10 @@ export default router;
 {
   "name": "Jorge"
   "email": "jorge@jorge.com"
-}
-*/
+  }
+  */
 
-router.put('/users/:id', async (req: Request, res: Response) => {
+router.put("/users/:id", async (req: Request, res: Response) => {
   try {
     // Obter o ID do usuário a partir dos parametros da requisição
     const { id } = req.params;
@@ -136,46 +132,86 @@ router.put('/users/:id', async (req: Request, res: Response) => {
     const userRepository = AppDataSource.getRepository(User);
 
     //Buscar o usuário no banco de dados
-    const user = await userRepository.findOneBy({id: parseInt(id)})
+    const user = await userRepository.findOneBy({ id: parseInt(id) });
 
     //Verificar se o usuário foi encontrado
     if (!user) {
       res.status(404).json({
-        message: 'Usuário não encontrado!',
-      })
-      return
-    } 
+        message: "Usuário não encontrado!",
+      });
+      return;
+    }
 
     //Verificar se já existe outro usuário com o mesmo email, mas que não seja o registro atual
     const existingUser = await userRepository.findOne({
       where: {
         email: data.email,
         id: Not(parseInt(id)), //Exclui o próprio resgistro da busca
-      }
-    })
+      },
+    });
 
     //Verificar se o usuário foi encontrado
     if (existingUser) {
       res.status(400).json({
-        message: 'Já existe um usuário com esse email!',
-      })
+        message: "Já existe um usuário com esse email!",
+      });
     }
 
     //Atualizar os dados do usuário
-    userRepository.merge(user, data)
+    userRepository.merge(user, data);
 
     //Salvar as alterações no banco de dados
-    const updateUser = await userRepository.save(user)
+    const updateUser = await userRepository.save(user);
 
     res.status(200).json({
-      message: 'Usuário atualizado com sucesso!',
+      message: "Usuário atualizado com sucesso!",
       data: updateUser,
-    })
+    });
+  } catch (error) {
+    //Retornar erro em caso de falha
+    res.status(500).json({
+      message: "Erro em editar o usuário!",
+    });
+  }
+});
+
+//Criar a rota para apagar usuário
+//Endereço para acessar a api atraves da aplicacao externa com o verbo PUT:http://localhost:8080/users/id
+
+router.delete("/users/:id", async (req: Request, res: Response) => {
+  try {
+    //Obter o id do usuário
+    const id = req.params.id;
+
+    //Obter o repositorio da entidade User
+    const userRepository = AppDataSource.getRepository(User);
+
+    //Buscar o usuário no banco de dados
+    const user = await userRepository.findOneBy({ id: parseInt(id) });
+
+    //Verificar se o usuário foi encontrado
+    if (!user) {
+      res.status(404).json({
+        message: "Usuário não encontrado!",
+      });
+      return;
+    }
+
+    //Remover o usuário do banco de dados
+    await userRepository.delete(user);
+    res.status(200).json({
+      message: "Usuário deletado com sucesso!",
+      data: user,
+    });
 
   } catch (error) {
     //Retornar erro em caso de falha
     res.status(500).json({
-      message: 'Erro em editar o usuário!',
-    })
+      message: "Erro em deletar o usuário!",
+    });
   }
-})
+
+});
+
+//Exportar a instrução que esta dentro da constante router
+export default router;
